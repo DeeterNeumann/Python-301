64f5111d81a176b6d5aae44f27cc33db9a870e86
Deeter Neumann <drneumann@uwalumni.com>
2025-03-02 20:36:11 -0800

    decorating decorator

--- 07_decorators/07_03_text_decoration.py
+++ 07_decorators/07_03_text_decoration.py
@@ -7,3 +7,26 @@
 # ******************************
 # Hello
 # ******************************
+
+# 30 asteriks
+
+def make_fancy(symbol):
+    def decorator_func(initial_func):
+        def wrapper(*args, **kwargs):
+            fancy = f"{symbol * 30}\n{initial_func(*args, **kwargs)}\n{symbol * 30}"
+            print(fancy)
+            return fancy
+        return wrapper
+    return decorator_func
+
+@make_fancy("*")
+def to_be_decorated(word):
+    return word
+
+@make_fancy("#")
+def decorate_again(word):
+    return word
+
+to_be_decorated("hello")
+
+decorate_again("dog")
