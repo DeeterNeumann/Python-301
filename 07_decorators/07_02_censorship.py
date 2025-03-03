723deffdd88b41691afa0cd1be925f8bdbe29410
Deeter Neumann <drneumann@uwalumni.com>
2025-03-02 22:07:34 -0800

    censor

--- 07_decorators/07_02_censorship.py
+++ 07_decorators/07_02_censorship.py
@@ -4,3 +4,24 @@
 #    "I bumped my toe! Shoot!"
 # Would, after decorating it with `@censor()`, return:
 #    "I bumped my toe! S****!"
+
+import re
+
+def censor(initial_func):
+    def wrapper(*args, **kwargs):
+        offensive_words = ["shoot", "crap"]
+        censored = initial_func(*args, **kwargs)
+        for word in offensive_words:
+            offensive_word = r'\b' + re.escape(word) + r'\b'
+            censored = re.sub(offensive_word, lambda match: match.group(0)[0] + "*" * (len(match.group(0))-1), censored)
+        print(censored)
+        return censored
+    return wrapper
+
+@censor            
+def offensive(something):
+    return something
+
+
+offensive("Oh shoot!")
+offensive("Oh crap!")
