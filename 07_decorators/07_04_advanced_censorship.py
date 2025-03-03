92a4acd8d0c969ab4eac549eb4d16a6397478ef0
Deeter Neumann <drneumann@uwalumni.com>
2025-03-02 21:54:37 -0800

    Expanded censorship

--- 07_decorators/07_04_advanced_censorship.py
+++ 07_decorators/07_04_advanced_censorship.py
@@ -1,3 +1,43 @@
 # Build on top of the censorship exercise and change your decorator function
 # so that you can pass the words it should censor when decorating a function, e.g.:
-# `@censor("shoot", "crab")` would censor the words "shoot" and "crab".
+# `@censor("shoot", "crap")` would censor the words "shoot" and "crap".
+
+import re
+
+def selective_censorship(*censored_words):
+    def censor(initial_func):
+        def wrapper(*args, **kwargs):
+            # offensive_words = ["shoot", "fuck", "shit", "damn"]
+            censored = initial_func(*args, **kwargs)
+            for word in censored_words:
+                offensive_word = r'\b' + re.escape(word) + r'\b'
+                censored = re.sub(offensive_word, lambda match: match.group(0)[0] + "*" * (len(match.group(0))-1), censored)
+            print(censored)
+            return censored
+        return wrapper
+    return censor
+
+@selective_censorship("shoot", "crap")            
+def offensive(something):
+    return something
+
+offensive("oh shoot! this is some crap!")
+
+
+
+# import re
+
+# def censor(initial_func):
+#     def wrapper(*args, **kwargs):
+#         offensive_words = ["shoot", "crap"]
+#         censored = initial_func(*args, **kwargs)
+#         for word in offensive_words:
+#             offensive_word = r'\b' + re.escape(word) + r'\b'
+#             censored = re.sub(offensive_word, lambda match: match.group(0)[0] + "*" * (len(match.group(0))-1), censored)
+#         print(censored)
+#         return censored
+#     return wrapper
+
+# @censor            
+# def offensive(something):
+#     return something
